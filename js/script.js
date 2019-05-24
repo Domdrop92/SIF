b55d002d6a898bbe7378d79f301108a5649cf539
Dominique Dixon <domdrop92@hotmail.com>
2019-05-23 19:59:52 -0700

    fixing up the task page and adding check button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-function checkOnIt() {
-    document.getElementById("demo").innerHTML = "check";
-}
+// function checkOnIt() {
+//     document.getElementById("demo").innerHTML = "check";
+    
+// }
 
-function submitIt() {
-    document.getElementById("demo").innerHTML = "send";
-}
+// function submitIt() {
+//     document.getElementById("demo").innerHTML = "send";
+// }
+
+var corrent1= "switch";
+var correct2= "case";
+var correct3= "case";
+
+
