6e3830a9fb3e4fae806eb5ae7477dafbf7054907
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2019-06-10 18:56:08 -0700

    changed logo

---
+++ js/insert.js
@@ -0,0 +1,10 @@
+var mysql= require ('mysql');
+var express= require('express');
+var app= express();
+var con= createConnection({
+    host: "localhost",
+    user: "root",
+    password: "#Kandie8788",
+    database: "sampledb"
+}); 
+connection.connect();
