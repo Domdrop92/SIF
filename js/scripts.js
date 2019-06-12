36c9aaeeb0e18e55fe53a28f8912d50b6ef5e683
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2019-06-11 18:45:36 -0700

    codemirror link added to button

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,14 @@ document.getElementById("myCalendar").onclick= function(){
     location.href = "calendar.html";
 };
 document.getElementById("myDashboard").onclick= function(){
-    location.href = "Dashboard.html";
+    location.href = "dashboard.html";
 };
 document.getElementById("myForum").onclick= function(){
     location.href = "forum.html";
 };
 document.getElementById("myInbox").onclick= function(){
     location.href = "inbox.html";
+};
+document.getElementById("myCodemirror").onclick= function(){
+    location.href = "codemirror.html";
 };
