8e3368399c9c1f6380d717018f354581b24831b4
Deidra22 <Deidra22@users.noreply.github.com>
2019-06-18 17:31:01 -0700

    got firestore registration to work just need to do login calling

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 // Firebase configuration
-let firebaseConfig = {
-  apiKey: "AIzaSyBfMKU92wbOsaux9enmoBN0xpdo3__de5E",
-  authDomain: "gitgirls-2019.firebaseapp.com",
-  databaseURL: "https://gitgirls-2019.firebaseio.com",
-  projectId: "gitgirls-2019",
-  storageBucket: "gitgirls-2019.appspot.com",
-  messagingSenderId: "924271804292",
-  appId: "1:924271804292:web:38987aee5f4e40d7"
-};
+// let firebaseConfig = {
+//   apiKey: "AIzaSyBfMKU92wbOsaux9enmoBN0xpdo3__de5E",
+//   authDomain: "gitgirls-2019.firebaseapp.com",
+//   databaseURL: "https://gitgirls-2019.firebaseio.com",
+//   projectId: "gitgirls-2019",
+//   storageBucket: "gitgirls-2019.appspot.com",
+//   messagingSenderId: "924271804292",
+//   appId: "1:924271804292:web:38987aee5f4e40d7"
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 console.log(firebase);
@@ -25,8 +25,8 @@ document.getElementById('registerForm').addEventListener('submit', formSubmit);
 function formSubmit(e) {
   e.preventDefault();
   // Get Values from the DOM
-  let firstName = document.querySelector('#firstNameInput').value;
-  let lastName = document.querySelector('#lastNameInput').value;
+  let first = document.querySelector('#firstNameInput').value;
+  let last = document.querySelector('#lastNameInput').value;
   let email = document.querySelector('#emailInput').value;
   let password = document.querySelector('#password', '#repeatePassword').value;
   //   let student = document.querySelector('#student').value;
@@ -35,28 +35,27 @@ function formSubmit(e) {
   //   Form Reset After Submission
   document.getElementById('registerForm').reset();
 
+  const userId = 
 
-  let userRef = db.collection('Users').doc('userInfo');
 
-  function sendMessage(firstName, lastName, email, password) {
-    let newFormMessage = formMessage.push();
-    newFormMessage.set({
-      name: name,
-      email: email,
-      password: password,
-    });
+  db.collection('Users').add({
+    first,
+    last, 
+    email,
+    password
+  }).then(res => {
+    console.log(res);
+  })
+}
 
-  let send = userRef.set({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-  }, { merge: true })
 
-    .then(function (success_value) {
-      console.log(success_value)
-    })
-    .catch(function (error_message) {
-      console.error("Error writing document: ", error_message);
-    })
-  }
+
+  // function sendMessage(first, last, email, password){
+  //   let newFormMessage = formMessage.push();
+  //   newFormMessage.add({
+  //     name: name,
+  //     email: email,
+  //     password: password
+  //   });
+  // }
+
