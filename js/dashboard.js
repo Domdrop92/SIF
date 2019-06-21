2a62f7122c2593ffe1491615c4686b36f823069a
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2019-06-20 18:18:14 -0700

    changed js file names and deleted node

--- js/dashboard.js
+++
@@ -1,6 +0,0 @@
-function upload(){
-    var imgcanvas= document.getElementById("can");
-    var filename= document.getElementById("finput");
-    var image= new SimpleImage(fileinput);
-    image.drawTo(imgcanvas);
-}
---
+++ js/dashboards.js
@@ -0,0 +1,15 @@
+document.getElementById("myCalendar").onclick= function(){
+    location.href = "calendar.html";
+};
+document.getElementById("myDashboard").onclick= function(){
+    location.href = "dashboard.html";
+};
+document.getElementById("myForum").onclick= function(){
+    location.href = "forum.html";
+};
+document.getElementById("myInbox").onclick= function(){
+    location.href = "inbox.html";
+};
+document.getElementById("myCodemirror").onclick= function(){
+    location.href = "codemirror.html";
+};
