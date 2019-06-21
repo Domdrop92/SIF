fc2e072b509e2a09af604931d51b274e127e4d31
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2019-06-20 18:14:00 -0700

    changed name of js file to task

--- js/task.js
+++ js/task.js
@@ -1,6 +1,12 @@
-// function upload(){
-//     var imgcanvas= document.getElementById("can");
-//     var filename= document.getElementById("finput");
-//     var image= new SimpleImage(fileinput);
-//     image.drawTo(imgcanvas);
-// }
+document.getElementById("myCalendar").onclick= function(){
+    location.href = "calendar.html";
+};
+document.getElementById("myDashboard").onclick= function(){
+    location.href = "Dashboard.html";
+};
+document.getElementById("myForum").onclick= function(){
+    location.href = "forum.html";
+};
+document.getElementById("myInbox").onclick= function(){
+    location.href = "inbox.html";
+};
