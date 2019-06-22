ebacee22cc9f65b9ad28516bdcdb61e4ee58f37b
Deidra22 <Deidra22@users.noreply.github.com>
2019-06-21 20:34:17 -0700

    cleaned up html and css needs review

--- js/app.js
+++ js/app.js
@@ -16,36 +16,6 @@ const db = firebase.firestore();
 
 console.log(firebase);
 
-//Reference for form collection
-// let formMessage = firebase.database().ref('Users');
-
-// //listen for submit event
-// document.getElementById('registerForm').addEventListener('submit', formSubmit);
-
-// //Submit form FOR REGISTRATION
-// function formSubmit(e) {
-//   e.preventDefault();
-//   // Get Values from the DOM
-//   let first = registerForm['firstNameInput'].value;
-//   let last = registerForm['lastNameInput'].value;
-//   let email = registerForm['emailInput'].value;
-//   let password = registerForm['password'].value;
-//   //   let student = document.querySelector('#student').value;
-//   //   let mentor = document.querySelector('#mentor').value;
-
-//   //   Form Reset After Submission
-//   document.getElementById('registerForm').reset();
-
-//   const userId = 
-//   db.collection('Users').add({
-//     first,
-//     last, 
-//     email,
-//     password
-//   }).then(res => {
-//     console.log(res);
-//   })
-// }
 
   // login
 //   const loginForm = document.querySelector('#login-form');
